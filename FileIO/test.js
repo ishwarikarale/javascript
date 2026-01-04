fbdca7475ba99593fab7ae6affbce96d49072e7b
Ishwari Karale <ishwarikarale2909@gmail.com>
2026-01-04 10:37:39 +0530

    Rename data file and updated test.js

--- FileIO/test.js
+++ FileIO/test.js
@@ -1,2 +1,22 @@
-var fs=require('fs');
-var filename="../data/ishwaridetails.txt";
+var fs = require('fs');
+var path=require('path');
+
+//fs.writeFileSync("ishwari.txt","Hello world this is Ishwari!",err=>{})
+var fileName = path.join(__dirname,'data','result.txt');
+
+//var onFileRead = function (err, data) {
+  //console.log("Data from File available");
+//console.log(data.toString());
+//};
+
+fs.readFile(fileName,function (err, data) {
+  if (err) {
+    console.log("Error:", err);
+    return;
+  }
+
+  console.log("Data from File available");
+  console.log(data.toString());
+});
+fs.appendFileSync(fileName," Hello fourth");
+console.log("this is the last line!");
